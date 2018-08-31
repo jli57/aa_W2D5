0e1668860a15d5481fce373c54cb24b2885559f4
Jingna <jingna13@gmail.com>
2018-08-31 16:30:50 -0700

    Implement Hashmap Data Structure

--- hash_map/lib/p05_hash_map.rb
+++ hash_map/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+  
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -9,49 +11,74 @@ class HashMap
   end
 
   def include?(key)
-    bucket = @store[key.hash % num_buckets]
+    bucket = bucket(key)
     bucket.include?(key)
   end
 
   def set(key, val)
-    bucket = @store[key.hash % num_buckets]
+    bucket = bucket(key)
     if bucket.include?(key)
       bucket.update(key, val)
     else
+      
+      if @count == num_buckets
+        resize!
+      end   
       bucket.append(key, val)
+      @count += 1 
     end
   end
 
   def get(key)
-    @store[key.hash % num_buckets].get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    bucket = bucket(key)
+    if bucket.include?(key)
+      
+      bucket.remove(key)
+      @count -= 1
+    end
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |node|
+        prc.call(node.key, node.val)
+      end 
+    end 
   end
 
+  
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
 
+
   private
+  
   def num_buckets
     @store.length
   end
 
   def resize!
+    new_hashmap_size = num_buckets * 2
+    new_buckets = Array.new(new_hashmap_size) { LinkedList.new }
+    self.each do |key, val|
+      new_buckets[ key.hash % (new_hashmap_size) ].append(key, val)
+    end
+    @store = new_buckets
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
